5072f4188d627457eb24b221bac3fe75a1e432de
Pamela <paamz.f@gmail.com>
2026-07-17 18:57:57 -0300

    adicionei novos testes de login

--- test/login.test.js
+++ test/login.test.js
@@ -15,5 +15,60 @@ describe('Login', () => {
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
         })
-    })
+
+        it('Deve retornar 400 quando o usuário não for informado', async () =>{
+            const bodyLogin = {...postLogin}
+            bodyLogin.username = ''
+
+            const resposta = await request(process.env.BASE_URL)
+            .post('/login')
+            .set('Content-Type', 'application/json')
+            .send(bodyLogin)
+            
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body.error).to.equal('Usuário e senha são obrigatórios.')
+        })
+
+        it('Deve retornar 400 quando a senha não for informada', async () =>{
+            const bodyLogin = {...postLogin}
+            bodyLogin.senha = ''
+
+            const resposta = await request(process.env.BASE_URL)
+            .post('/login')
+            .set('Content-Type', 'application/json')
+            .send(bodyLogin)
+            
+            expect(resposta.status).to.equal(400);
+            expect(resposta.body.error).to.equal('Usuário e senha são obrigatórios.')
+        })
+
+        it('Deve retornar 401 quando for informado um usuário incorreto ou invàlido', async () =>{
+            const bodyLogin = {...postLogin}
+            bodyLogin.username = 'junior'
+            bodyLogin.senha = '123456'
+
+            const resposta = await request(process.env.BASE_URL)
+            .post('/login')
+            .set('Content-Type', 'application/json')
+            .send(bodyLogin)
+            
+            expect(resposta.status).to.equal(401);
+            expect(resposta.body.error).to.equal('Usuário ou senha inválidos.')
+        })
+
+        it('Deve retornar 401 quando for informada uma senha incorreta ou invàlida', async () =>{
+            const bodyLogin = {...postLogin}
+            bodyLogin.username ='julio.lima'
+            bodyLogin.senha = '12345'
+
+            const resposta = await request(process.env.BASE_URL)
+            .post('/login')
+            .set('Content-Type', 'application/json')
+            .send(bodyLogin)
+            
+            console.log(resposta.body)
+            expect(resposta.status).to.equal(401);
+            expect(resposta.body.error).to.equal('Usuário ou senha inválidos.')
+        })
 } )
+})
