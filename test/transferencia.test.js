05e616275325fe5d5ee0987b6ab7fbab4e0e0cff
Pamela <paamz.f@gmail.com>
2026-07-17 16:51:45 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao');
+const postTransferencias = require ('../fixtures/postTransferencias.json')
+
 
 describe('Transferências', () => {
     describe('POST /transferencias', () => {
@@ -11,31 +13,26 @@ describe('Transferências', () => {
             token = await obterToken('julio.lima', '123456')
         })
         it('Deve retornar sucesso com 201 quando o valor da transferência for acima de R$10,00', async () => {
+            const bodyTransferencias = {...postTransferencias}
+            
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 2,
-                    contaDestino: 1,
-                    valor: 20.00,
-                    token: ''
-                });
+                .send(bodyTransferencias);
             console.log('Status da transferência:', resposta.status);
             console.log('Resposta:', resposta.body);    
         });
 
         it('Deve retornar falha com 422 quando o valor da transferência for abaixo de R$10,00', async () => {
+            const bodyTransferencias = {...postTransferencias}
+            bodyTransferencias.valor = 7
+
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 2,
-                    contaDestino: 1,
-                    valor: 9.99,
-                    token: ''
-                });
+                .send(bodyTransferencias);
             console.log('Status da transferência:', resposta.status);
             console.log('Resposta:', resposta.body);                  
         });
